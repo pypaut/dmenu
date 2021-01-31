18191afe2a8c3dfdced138c59e20779044dcf931
Geoffrey Jount <geoffrey.jount@epita.fr>
2021-01-31 01:09:06 +0100

    Bigger font

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Nerd Font Mono:style=Medium:size=10"
+	"Iosevka Nerd Font Mono:style=Medium:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
