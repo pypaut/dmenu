e462d8cebafc4c6a9fd39c78a3c12cc3705bec72
Geoffrey <pypaut@hotmail.fr>
2021-12-23 17:06:15 +0100

    Dracula colors & new layout

--- config.def.h
+++ config.def.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Nerd Font Mono:style=Medium:size=11"
+	"Iosevka Nerd Font Mono:style=Medium:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
@@ -41,14 +41,36 @@ static const char nord14[] = "#a3be8c";  /* Green */
 static const char nord15[] = "#b48ead";  /* Purple */
 static const char blackblue[] = "#0e1420";  /* Black blue */
 
+// static const char *colors[SchemeLast][2] = {
+// 	/*     fg         bg       */
+// 	[SchemeNorm] = { color1, blackblue },
+// 	[SchemeSel]  = { color2, nord10 },
+// 	[SchemeOut]  = { color3, color4 },
+// };
+
+/* Dracula */
+static const char drac_background[]  = "#282a36";
+static const char drac_currentline[] = "#44475a";
+static const char drac_selection[]   = "#44475a";
+static const char drac_foreground[]  = "#f8f8f2";
+static const char drac_comment[]     = "#6272a4";
+static const char drac_cyan[]        = "#8be9fd";
+static const char drac_green[]       = "#50fa7b";
+static const char drac_orange[]      = "#ffb86c";
+static const char drac_pink[]        = "#ff79c6";
+static const char drac_purple[]      = "#bd93f9";
+static const char drac_red[]         = "#ff5555";
+static const char drac_yellow[]      = "#f1fa8c";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { color1, blackblue },
-	[SchemeSel]  = { color2, nord10 },
+	[SchemeNorm] = { color2, drac_background },
+	[SchemeSel]  = { drac_purple, drac_background },
 	[SchemeOut]  = { color3, color4 },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines = 5;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -57,4 +79,4 @@ static unsigned int lines = 5;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 3;
+static const unsigned int border_width = 0;
