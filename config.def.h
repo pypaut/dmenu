486b6e968706b52acb09eeb5fe949c86476cf186
Geoffrey JOUNT <gjount@scaleway.com>
2023-08-12 09:18:49 +0200

    Aesthetic tweaks
    Rewrite colors, change font size and use Nord.

--- config.def.h
+++ config.def.h
@@ -3,13 +3,13 @@
 
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 200;                 /* minimum width when centered */
+static int min_width = 100;                 /* minimum width when centered */
 static const unsigned int border_width = 2; /* border width */
 static int centered_text = 1;               /* center text inside window */
 static int show_cursor = 1;                 /* show cursor in input field */
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines = 1;
+static unsigned int lines = 3;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -20,7 +20,7 @@ static const char worddelimiters[] = " ";
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "Iosevka Nerd Font Mono:style=Medium:size=12"
+    "Iosevka:style=Medium:size=30"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
@@ -29,39 +29,31 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 /**********/
 
 /* Defaults */
-static const char color1[] = "#bbbbbb";
-static const char color2[] = "#eeeeee";
-static const char color3[] = "#000000";
-static const char color4[] = "#00ffff";
-/* Polar Night */
-static const char nord0[]  = "#2e3440";
-static const char nord1[]  = "#3b4252";
-static const char nord2[]  = "#434c5e";
-static const char nord3[]  = "#4c566a";
-/* Snow Storm */
-static const char nord4[]  = "#d8dee9";
-static const char nord5[]  = "#e5e9f0";
-static const char nord6[]  = "#eceff4";
-/* Frost */
-static const char nord7[]  = "#8fbcbb";
-static const char nord8[]  = "#88c0d0";
-static const char nord9[]  = "#81a1c1";
-static const char nord10[] = "#5e81ac";
+static const char color1[]    = "#bbbbbb";
+static const char color2[]    = "#eeeeee";
+static const char color3[]    = "#000000";
+static const char color4[]    = "#00ffff";
+/* Polar Night : dark blue */
+static const char polar1[]    = "#2e3440";
+static const char polar2[]    = "#3b4252";
+static const char polar3[]    = "#434c5e";
+static const char polar4[]    = "#4c566a";
+/* Snow Storm : white */
+static const char snow1[]     = "#d8dee9";
+static const char snow2[]     = "#e5e9f0";
+static const char snow3[]     = "#eceff4";
+/* Frost : blue */
+static const char frost1[]    = "#8fbcbb";
+static const char frost2[]    = "#88c0d0";
+static const char frost3[]    = "#81a1c1";
+static const char frost4[]    = "#5e81ac";
 /* Aurora */
-static const char nord11[] = "#bf616a";  /* Red */
-static const char nord12[] = "#d08770";  /* Orange */
-static const char nord13[] = "#ebcb8b";  /* Yellow */
-static const char nord14[] = "#a3be8c";  /* Green */
-static const char nord15[] = "#b48ead";  /* Purple */
+static const char aurora1[]   = "#bf616a";  /* Red */
+static const char aurora2[]   = "#d08770";  /* Orange */
+static const char aurora3[]   = "#ebcb8b";  /* Yellow */
+static const char aurora4[]   = "#a3be8c";  /* Green */
+static const char aurora5[]   = "#b48ead";  /* Purple */
 static const char blackblue[] = "#0e1420";  /* Black blue */
-
-// static const char *colors[SchemeLast][2] = {
-// 	/*     fg         bg       */
-// 	[SchemeNorm] = { color1, blackblue },
-// 	[SchemeSel]  = { color2, nord10 },
-// 	[SchemeOut]  = { color3, color4 },
-// };
-
 /* Dracula */
 static const char drac_background[]  = "#282a36";
 static const char drac_currentline[] = "#44475a";
@@ -76,11 +68,20 @@ static const char drac_purple[]      = "#bd93f9";
 static const char drac_red[]         = "#ff5555";
 static const char drac_yellow[]      = "#f1fa8c";
 
+/* Nord */
 static const char *colors[SchemeLast][2] = {
-    /*     fg         bg       */
-    [SchemeNorm]   = { color2, drac_background },
-    [SchemeSel]    = { drac_purple, drac_background },
-    [SchemeOut]    = { color3, color4 },
-    [SchemeBorder] = { drac_purple, drac_purple },
+	/*     fg         bg       */
+	[SchemeNorm] = { color1, blackblue },
+	[SchemeSel]  = { color2, blackblue },
+	[SchemeOut]  = { snow3, snow3 },
+	[SchemeBorder] = { frost3, frost3 },
 };
 
+/* Dracula */
+// static const char *colors[SchemeLast][2] = {
+//     /*     fg         bg       */
+//     [SchemeNorm]   = { color2, drac_background },
+//     [SchemeSel]    = { drac_purple, drac_background },
+//     [SchemeOut]    = { color3, color4 },
+//     [SchemeBorder] = { drac_purple, drac_purple },
+// };
